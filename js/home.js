7f39c54f7e86a0a1a6bc0cf6dc7cd598252342a9
Shakila Zumur <shakilazumur@gmail.com>
2025-05-17 21:16:25 +0300

    added newbalance and account

--- js/home.js
+++ js/home.js
@@ -5,7 +5,8 @@
 
 // add money to the account
 // step-1: add an event handler to the add money button inside the form
-document.getElementById('btn-add-money').addEventListener('click', function(event){
+document.getElementById('btn-add-money')
+    .addEventListener('click', function (event) {
         // prevent page reload after form submit
         event.preventDefault();
 
@@ -15,4 +16,26 @@ document.getElementById('btn-add-money').addEventListener('click', function(even
         // get the pin number provided
         const pinNumberInput = document.getElementById('input-pin-number').value;
         console.log(pinNumberInput);
+
+        // step-3: veeify pin number
+        // wromg way to validate pin number
+        if(pinNumberInput === '1234'){
+            console.log('adding money to your account');
+
+            //  step-4: get the current balance
+            const balance = document.getElementById('account-balance').innerText;
+            console.log(typeof balance)
+
+            // step-5: add addMoneyInput with balance
+            const addMoneyNumber = parseFloat(addMoneyInput);
+            const balanceNumber = parseFloat(balance);
+            const newBalance = addMoneyNumber + balanceNumber;
+            console.log(newBalance);
+
+            // step-6: update the balance in the UI/DOM
+            document.getElementById('account-balance').innerText = newBalance;
+        }
+        else{
+            alert('Failed to add money! Please try again')
+        }
     });
